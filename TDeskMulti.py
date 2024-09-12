b5b4f4594b83f6de2b47f160902f62ad45a6e75f
Vitaliy Hrachov <vitlilg@gmail.com>
2024-09-12 18:46:44 +0300

    new

--- TDeskMulti.py
+++ TDeskMulti.py
@@ -390,6 +390,7 @@ create_context_menu(input_widget)
 
 # Привязываем событие Ctrl + V для вставки
 input_widget.bind("<Control-v>", lambda e: input_widget.event_generate('<<Paste>>'))
+input_widget.bind("<Control-м>", lambda e: input_widget.event_generate('<<Paste>>'))
 
 while True:
     event, values = window.read()
